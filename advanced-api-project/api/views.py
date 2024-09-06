12a16d99c933bc2cb0400d47ee169571bd5730d3
Oby Tonia <anthonian22@gmail.com>
2024-09-06 18:47:25 +0100

    mod.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -5,6 +5,7 @@ from rest_framework import generics, permissions
 from .models import Book
 from .serializers import BookSerializer
 
+
 class BookListView(generics.ListCreateAPIView):
     """
     List all books or create a new book.
@@ -16,7 +17,19 @@ class BookListView(generics.ListCreateAPIView):
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     """ Retrieve, update, or delete a book by ID.
     """
-
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [permissions.IsAuthenticated]  # Restrict access to authenticated users
+
+class BookCreateView(generics.CreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookUpdateView(generics.UpdateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    lookup_field = 'pk'
+
+class DeleteView(generics.DestroyAPIView):
+    queryset = Book.objects.all()
+    lookup_field = 'pk'
