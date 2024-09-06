28cd873f86fdccaba6f4c38048580954c79520fe
Oby Tonia <anthonian22@gmail.com>
2024-09-06 19:52:15 +0100

    mod.

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -88,7 +88,11 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    },
+    'test': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'my_test_database.sqlite3',
+}
 }
 
 
