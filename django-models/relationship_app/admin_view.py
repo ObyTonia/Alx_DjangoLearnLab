4ffcfb6a68b3339af981bd9b877f6a8537898e2e
Oby Tonia <anthonian22@gmail.com>
2024-09-06 20:30:07 +0100

    mod.

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -3,9 +3,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
-def is_admin(user):
-    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
-
-@user_passes_test(is_admin)
+@user_passes_test(lambda user: user.profile.role == 'Admin')
 def admin_view(request):
+    # Admin-specific content here
     return render(request, 'admin_template.html')
