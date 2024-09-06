3db3ea6c4d5e068c8d4473e941496cb988b1eb47
Oby Tonia <anthonian22@gmail.com>
2024-09-06 18:52:56 +0100

    mod

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('books/',BookListView.as_view(), name='book-list'), # Handles GET (list) and POST (create)
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'), # Handles GET (retrieve), PUT (update), DELETE (delete)
     path('books/create/', BookCreateView.as_view(), name='book-create'),
-    path('books/<int:pk>/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/update/', BookUpdateView.as_view(), name='book-update'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
