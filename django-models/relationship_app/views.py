34da54b9fbac8533f4d32854051e8c27819418ee
Oby Tonia <anthonian22@gmail.com>
2024-09-06 16:39:01 +0100

    mod view.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Book  # Import your Book model
+
+def list_books(request):
+  books = Book.objects.all()  # Fetch all books from database
+  context = {'books': books}  # Create a context dictionary
+  return render(request, 'list_books.html', context)  # Render template with context
+
+from django.views.generic import DetailView
+from .models import Library
+
+class LibraryDetailView(DetailView):
+  model = Library  # Specify the model for the view
+  template_name = 'library_detail.html'  # Set the template for rendering
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context['books'] = self.object.books.all()  # Access library's books
+    return context
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+  path('books/', views.list_books, name='list_books'),
+  path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
+]
