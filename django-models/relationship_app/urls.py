2d90c92989806e7cb2e8f77bccdef8669da6ab8b
Oby Tonia <anthonian22@gmail.com>
2024-09-06 17:03:47 +0100

    mod

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -1,3 +1,11 @@
+from django.urls import path
+from .views import list_books, LibraryDetailView
+
+urlpatterns = [
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+]
+
 from django.contrib import admin
 from django.urls import path, include
 
--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -15,16 +15,3 @@ from .models import Library
 class LibraryDetailView(DetailView):
   model = Library  # Specify the model for the view
   template_name = 'relationship_app/library_detail.html'  # Set the template for rendering
-
-  def get_context_data(self, **kwargs):
-    context = super().get_context_data(**kwargs)
-    context['books'] = self.object.books.all()  # Access library's books
-    return context
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-  path('books/', views.list_books, name='list_books'),
-  path('libraries/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
