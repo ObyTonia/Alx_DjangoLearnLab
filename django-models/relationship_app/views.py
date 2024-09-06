2d08d20cd369105dacfcd41aeb4a23c1e806a0bc
Oby Tonia <anthonian22@gmail.com>
2024-09-06 20:38:10 +0100

    mod

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -34,10 +34,29 @@ def register(request):
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
 
-# admin_view.py
+
+
+# Role views
 from django.contrib.auth.decorators import user_passes_test
 
-@user_passes_test(lambda user: user.profile.role == 'Admin')
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+@user_passes_test(is_admin)
 def admin_view(request):
-    # Admin-specific content here
-    return render(request, 'admin_template.html')
+    return render(request, 'admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
+
