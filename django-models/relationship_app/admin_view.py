c8ac83d4afdafd25f8e055f61e44502514823a22
Oby Tonia <anthonian22@gmail.com>
2024-09-12 11:05:29 +0100

    updates

--- django-models/relationship_app/admin_view.py
+++ django-models/relationship_app/admin_view.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 
 # Check if the user has the 'Admin' role
-def is_admin(user):
+def Admin(user):
     return user.userprofile.role == 'Admin'
 
 @user_passes_test(lambda user: user.profile.role == 'Admin')
