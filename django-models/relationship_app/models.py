c24bd0a9a04672ad8c21c7816257fb30a7aee15e
Oby Tonia <anthonian22@gmail.com>
2024-09-12 11:02:15 +0100

    updates

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -27,6 +27,20 @@ def create_user_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
     instance.userprofile.save()
+
+
+"ADMIN_VIEW"
+from django.shortcuts import render
+from django.contrib.auth.decorators import user_passes_test
+
+# Check if the user has the 'Admin' role
+def is_admin(user):
+    return user.userprofile.role == 'Admin'
+
+@user_passes_test(lambda user: user.profile.role == 'Admin')
+def admin_view(request):
+    # Admin-specific content here
+    return render(request, 'admin_view.html')
     
 "BOOKSHELF"
 class Author(models.Model):
