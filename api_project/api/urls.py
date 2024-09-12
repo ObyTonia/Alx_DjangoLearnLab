fc4be4ba45f6ac5829029cb6c91d159e9fba0db8
Oby Tonia <anthonian22@gmail.com>
2024-09-12 10:25:32 +0100

    updates

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,23 @@
 from django.urls import path
 from .views import BookList
+from rest_framework.authtoken.views import obtain_auth_token
+
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+]
+
+"ROUTERSETUP"
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet
+
+# Create a router and register our viewset with it.
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),  # Includes all routes registered with the router
 ]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,3 +9,28 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
   # Retrieve all books
     serializer_class = BookSerializer  # Use BookSerializer for serialization
+
+    "BOOKVIEWSET"
+from rest_framework import permissions
+from rest_framework.viewsets import ModelViewSet
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]  # Restrict access to authenticated users
+
+"CUSTOM PERMISSION"
+from rest_framework.permissions import BasePermission
+
+class IsAdminOrReadOnly(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_authenticated and request.user.is_staff 
+
+
+class BookViewSet(ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAdminOrReadOnly]
+  # Allow admin or read-only
