55421e4702ed5124861e72312e346dd40ce01dce
Oby Tonia <anthonian22@gmail.com>
2024-09-12 12:57:34 +0100

    updates

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -8,3 +8,19 @@ class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'publication_year')
     list_filter = ('author', 'publication_year')
     search_fields = ('title', 'author')
+
+"CUSTOM USER"
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import CustomUser 
+
+class CustomUserAdmin(BaseUserAdmin):
+    model = CustomUser
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        (_('Personal info'), {'fields': ('date_of_birth', 'profile_photo')}),
+        (_('Permissions'), {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
+    )
+
+admin.site.register(CustomUser, CustomUserAdmin)
--- advanced_features_and_security/LibraryProject/relationship_app/admin.py
+++ advanced_features_and_security/LibraryProject/relationship_app/admin.py
@@ -1,17 +1,3 @@
 from django.contrib import admin
 
-# Register your models here.
-from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-from django.utils.translation import gettext_lazy as _
-from .models import CustomUser 
-
-class CustomUserAdmin(BaseUserAdmin):
-    model = CustomUser
-    fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
-        (_('Personal info'), {'fields': ('date_of_birth', 'profile_photo')}),
-        (_('Permissions'), {'fields': ('is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
-    )
-
-admin.site.register(CustomUser, CustomUserAdmin)
+# Register your models here:
