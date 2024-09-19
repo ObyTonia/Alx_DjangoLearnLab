a29f131833cb494217d7232037242d138ec0840c
Oby Tonia <anthonian22@gmail.com>
2024-09-19 11:42:04 +0100

    mod. serializers in account app

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,11 +1,9 @@
-from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
+from .models import CustomUser
+from django.contrib.auth import get_user_model, authenticate
 
 class UserSerializer(serializers.ModelSerializer):
-    # Explicitly defining the password field
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
-
     class Meta:
         model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
@@ -13,15 +11,30 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         # Create user with hashed password
-        user = get_user_model().objects.create_user(
-            username=validated_data['username'],
-            email=validated_data['email'],
-            password=validated_data['password'],
-            bio=validated_data.get('bio', ''),
-            profile_picture=validated_data.get('profile_picture', None),
-        )
+        user = get_user_model().objects.create_user(**validated_data)
         
-        # Create token for user
+        # Create authentication token for the user
         Token.objects.create(user=user)
         
         return user
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = CustomUser.objects.create_user(validated_data['username'], validated_data['email'], validated_data['password'])
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect credentials")
