ad325a919a0509c777f7f053ba259412cdbe85df
Oby Tonia <anthonian22@gmail.com>
2024-09-19 11:34:03 +0100

    mod. serializers

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,18 +2,26 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
-# Custom user serializer for user registration
 class UserSerializer(serializers.ModelSerializer):
+    # Explicitly defining the password field
+    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+
     class Meta:
         model = get_user_model()
-        fields = ['id', 'username', 'password', 'email', 'bio', 'profile_picture']  # Add other fields if necessary
+        fields = ['id', 'username', 'email', 'bio', 'profile_picture', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Create a new user using the create_user method
-        user = get_user_model().objects.create_user(**validated_data)
+        # Create user with hashed password
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture', None),
+        )
         
-        # Create the token for the new user
+        # Create token for user
         Token.objects.create(user=user)
         
         return user
