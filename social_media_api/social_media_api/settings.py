af8d5769a1391fa168d2111bce1e85dd01d7ba45
Oby Tonia <anthonian22@gmail.com>
2024-09-21 09:57:07 +0100

    updates

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -96,9 +96,14 @@ WSGI_APPLICATION = 'social_media_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'toniadb',
+        'USER': 'root',
+        'PASSWORD': 'anthonian22@gmail.com',
+        'HOST': 'localhost',  # or your MySQL server IP
+        'PORT': '3306',  # default MySQL port
     }
+    
 }
 
 
